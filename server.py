65d43e072bd769bd061ba706df4189c74a230c5b
Argho Roy <argho@ualberta.ca>
2022-11-08 20:54:44 -0700

    Assignment 4 completed

--- server.py
+++ server.py
@@ -22,7 +22,7 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 import json
 app = Flask(__name__)
 app.debug = True
@@ -36,6 +36,7 @@ app.debug = True
 class World:
     def __init__(self):
         self.clear()
+        self.count = 0
         
     def update(self, entity, key, value):
         entry = self.space.get(entity,dict())
@@ -74,27 +75,32 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect('/static/index.html')   #Reference: https://vegibit.com/how-to-redirect-to-url-in-python-flask/#:~:text=The%20redirect()%20function%20allows,some%20text%20to%20the%20user.
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+    current_entry = flask_post_json()
+    for key in current_entry:
+        myWorld.update(entity, key, current_entry[key])
+        
+
+    return myWorld.get(entity)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return myWorld.world()
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return myWorld.get(entity)
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return myWorld.world()
 
 if __name__ == "__main__":
     app.run()
